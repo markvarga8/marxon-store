f27c8560ecbf50186f9f4adcf8b88ae03093d53d
Márk <mark.varga@tmrw.life>
2023-08-04 17:52:29 +0200

    featured products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,26 @@
-const HomePage = () => {
+import getBillboards from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const product = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("27975d00-d493-492a-8f42-885c465bce79");
+
+  console.log(product);
+
   return (
-    <div>
-      <h3>This is the home page</h3>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={product} />
+      </div>
+    </Container>
   );
 };
 
