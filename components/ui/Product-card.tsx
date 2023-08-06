4d352ad2f4707ce626a216bf9e919af5c345ae1d
Márk <mark.varga@tmrw.life>
2023-08-06 21:51:39 +0200

    fixed small fix

--- components/ui/Product-card.tsx
+++ components/ui/Product-card.tsx
@@ -23,15 +23,17 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     router.push(`product/${data?.id}`);
   };
 
-  const onPreview: MouseEvent<HTMLButtonElement> = (event) => {
+  const onPreview: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void = (event) => {
     event.stopPropagation();
-
     previewModal.onOpen(data);
   };
 
-  const onAddToCart: MouseEvent<HTMLButtonElement> = (event) => {
+  const onAddToCart: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void = (event) => {
     event.stopPropagation();
-
     cart.addItem(data);
   };
 
@@ -52,11 +54,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={onPreview}
+              onClick={() => onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={onAddToCart}
+              onClick={() => onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
