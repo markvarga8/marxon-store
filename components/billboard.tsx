d79ca73ddef2cb8b0b95154075d5a1e3eac2deab
Márk <mark.varga@tmrw.life>
2023-08-13 12:54:31 +0200

    main: Fixed billboards

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,11 +8,11 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
-        className="rounded-xl relative aspect-square md:apect-[2.4/1] overflow-hidden bg-cover"
+        className="rounded-xl relative aspect-square md:apect-[2.4/1] overflow-hidden bg-cover h-72 w-full"
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-full max-w-xs">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-full max-w-xs bg-gray-300 opacity-80 p-3 rounded-md">
             {data?.label}
           </div>
         </div>
